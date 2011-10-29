7f28779a42c0c04a6d81a713da4d188b5fdfe05c
IMANARI Tomohiko <t1535c75@gmail.com>
2011-10-29 13:43:56 +0900

    initial commit

---
+++ HelloGit/HelloGit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloGit
+//
+//  Created by Tomohiko IMANARI on 11/10/29.
+//  Copyright (c) 2011年 CSFactory. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
